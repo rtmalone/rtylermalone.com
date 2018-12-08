e6b25a3a9d9d2ee310d4ffadee786e2c9341ad9a
Tyler Malone <tyler@pylon.com>
2018-12-07 21:18:26 -0500

    profile pic and name refactor

--- src/components/Temp/index.js
+++ src/components/Temp/index.js
@@ -6,14 +6,23 @@ import profilePic from "../../static/images/profile-pic.jpg";
 const Temp = () => {
   return (
     <div className={styles.temp}>
-      <h1>Tyler Malone</h1>
+      <div className={styles.heading}>
+        <img
+          className={styles.profilePic}
+          src={profilePic}
+          alt="Tyler Malone"
+        />
+        <div>
+          <h1>Tyler</h1>
+          <h1>Malone</h1>
+        </div>
+      </div>
       <p className={styles.shortBio}>
         Tyler is a web engineer based in Chattanooga, TN building
         <br />
         solutions for AI with React and React Native at{" "}
         <a href="https://pylon.com">Pylon ai</a>
       </p>
-      <img className={styles.profilePic} src={profilePic} alt="Tyler Malone" />
     </div>
   );
 };
