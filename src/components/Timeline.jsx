30e5ec96c3f63dc3b60c7bed1b76a3d79606da1f
Tyler Malone <rtylermalone@gmail.com>
2021-09-18 16:15:57 -0400

    Timeline nav improvements

--- src/components/Timeline.jsx
+++ src/components/Timeline.jsx
@@ -5,8 +5,13 @@ export default function Timeline({careerData, setStep, step}) {
     const careerLength = careerData.length;
     const [translate, setTranslate] = useState(25);
     const dateList = careerData.map((item, i) => 
-        <li><a href="#0" key={i} onClick={() => setStep(i)} className={`${styles.date} ${step >= i ? styles.dateSelected : ''}`}>{item.date}</a></li>
+        <li key={i}><a href="#0" key={i} onClick={() => setStep(i)} className={`${styles.date} ${step >= i ? styles.dateSelected : ''}`}>{item.date}</a></li>
     );
+
+    function timelineClick(e, value) {
+        e.preventDefault();
+        setTranslate(value);
+    }
 
     return (
         <section className={`${styles.timeline}`}>
@@ -23,8 +28,9 @@ export default function Timeline({careerData, setStep, step}) {
                 </div>
 
                 <ul>
-                    <li><a onClick={() => setTranslate(0)} href="#0" className={`${styles.textReplace} ${styles.nav} ${styles.navPrev} ${translate === 0 ? styles.navInactive : ''}`}>Prev</a></li>
-                    <li><a onClick={() => setTranslate(25)} href="#0" className={`${styles.textReplace} ${styles.nav} ${styles.navNext} ${translate === 25 ? styles.navInactive : ''}`}>Next</a></li>
+                    {/* TODO: Make these btns, silly. */}
+                    <li><a onClick={(e) => timelineClick(e, 0)} href="#" className={`${styles.textReplace} ${styles.nav} ${styles.navPrev} ${translate === 0 ? styles.navInactive : ''}`}>Prev</a></li>
+                    <li><a onClick={(e) => timelineClick(e, 25)} href="#" className={`${styles.textReplace} ${styles.nav} ${styles.navNext} ${translate === 25 ? styles.navInactive : ''}`}>Next</a></li>
                 </ul>
             </div>
         </section>
