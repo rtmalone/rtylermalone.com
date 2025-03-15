330e0fadc241d5302ac251499e506a1229e8b6bb
Tyler Malone <rtmalone@users.noreply.github.com>
2025-03-15 13:21:42 -0400

    Add new job data

--- src/data/employment.js
+++ src/data/employment.js
@@ -1,11 +1,33 @@
+// if less than 1 year, set employmentYears to 0
+
 export const employmentHistory = [
+  {
+    id: "citir",
+    company: "Citiri",
+    title: "Senior Software Engineer",
+    startYear: 2025,
+    employmentYears: 0,
+    stack: ["Lightning Web Components", "Lightning Aura", "Apex", "D3.js", "DHTMLX Gantt"],
+    summary: "Developing owner-scope project management tools for critical infrastructure.",
+    description: "Bringing a 2GP package to feature parity with an older package in order to go to market with a modular solution. Alongside the development of the new package, I'm also maintaining the legacy codebase by addressing bugs that affect current customers."
+  },
+  {
+    id: "thrivent",
+    company: "Thrivent Financial",
+    title: "Contract Engineer",
+    startYear: 2024,
+    employmentYears: 0,
+    stack: ["Lightning Web Components", "OmniStudio", "Apex", "Github Actions"],
+    summary: "Building a new Financial Advisor experience on Salesforce while mentoring junior developers.",
+    description: "Using OmniStudio and custom LWCs to build an extensive Financial Advisor dashboard combining protfolio, product, and client data from external sources across the entire Thrivent organization. Implemented HMAC security to access an external app producing client facing PDFs."
+  },
   {
     id: "codescience",
     company: "CodeScience",
     title: "Web Engineer",
     startYear: 2020,
     employmentYears: 4,
-    stack: ["Lightning Web Components", "Angular", "Apex", "Salesforce"],
+    stack: ["Lightning Web Components", "Lightning Aura","Apex", "SF CLI"],
     summary: "Building solutions for Salesforce with Lightning Web Components, Angular & Apex at CodeScience.",
     description: "Working on complex Salesforce implementations, building custom UI components and integrations. Developing Lightning Web Components and leveraging the Salesforce platform to create scalable solutions for enterprise clients."
   },
@@ -25,7 +47,7 @@ export const employmentHistory = [
     title: "Software Engineer",
     startYear: 2015,
     employmentYears: 2,
-    stack: ["React", "Firebase", "Node.js", "Google Cloud Functions", "Material-UI"],
+    stack: ["React", "Firebase", "Node.js", "Google Cloud Functions", "Material-UI", "AngularJS"],
     summary: "Built a social platform for sports fans and a user-powered broadcasting platform.",
     description: "Developed a real-time social platform connecting sports fans during live events. Created a broadcasting platform allowing users to share their own sports commentary and analysis. Implemented responsive designs for mobile and desktop users."
   },
